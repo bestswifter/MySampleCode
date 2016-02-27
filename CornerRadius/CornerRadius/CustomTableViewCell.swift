7a0c85cc96e83e4d857dffdb36195d4209211776
张星宇 <649395594@qq.com>
2016-02-27 22:48:50 +0800

    高效 CornerRadius 研究

---
+++ CornerRadius/CornerRadius/CustomTableViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  CustomTableViewCell.swift
+//  CornerRadius
+//
+//  Created by 张星宇 on 16/2/27.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    let imgView1 = UIImageView(frame: CGRectMake(20, 20, 60, 60))
+    let imgView2 = UIImageView(frame: CGRectMake(100, 20, 60, 60))
+    let imgView3 = UIImageView(frame: CGRectMake(180, 20, 60, 60))
+    let imgView4 = UIImageView(frame: CGRectMake(260, 20, 60, 60))
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        imgView1.layer.cornerRadius = 30
+        imgView1.layer.masksToBounds = true
+        self.contentView.addSubview(imgView1)
+        
+        imgView2.layer.cornerRadius = 30
+        imgView2.layer.masksToBounds = true
+        
+        imgView3.layer.cornerRadius = 30
+        imgView3.layer.masksToBounds = true
+        
+        imgView4.layer.cornerRadius = 30
+        imgView4.layer.masksToBounds = true
+        
+        self.contentView.addSubview(imgView2)
+        self.contentView.addSubview(imgView3)
+        self.contentView.addSubview(imgView4)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setupContent(imgName imgName: String) {
+        imgView1.image = UIImage(named: imgName)
+        imgView2.image = UIImage(named: imgName)
+        imgView3.image = UIImage(named: imgName)
+        imgView4.image = UIImage(named: imgName)
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CornerRadius/CornerRadius/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  CornerRadius
+//
+//  Created by 张星宇 on 16/2/27.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let table = UITableView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width, UIScreen.mainScreen().bounds.size.height))
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        view.addSubview(table)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifier = "tableviewcell"
+        var cell = table.dequeueReusableCellWithIdentifier(identifier) as? CustomTableViewCell
+        if cell == nil {
+            cell = CustomTableViewCell(style: .Default, reuseIdentifier: identifier)
+            cell?.selectionStyle = .None
+        }
+        
+        cell?.setupContent(imgName: String(indexPath.row + 1))
+        
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
