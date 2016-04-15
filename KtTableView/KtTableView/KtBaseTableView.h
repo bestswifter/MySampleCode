d2a82387fd90b2c9191866dc1f800252396d7aa4
zhangxingyu <zhangxingyu@baidu.com>
2016-04-15 23:59:46 +0800

    基类TableView 和TableViewDelegate改造
    实际使用

---
+++ KtTableView/KtTableView/KtBaseTableView.h
@@ -0,0 +1,36 @@
+//
+//  KtBaseTableView.h
+//  KtTableView
+//
+//  Created by baidu on 16/4/13.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KtTableViewDataSource.h"
+
+@class KtTableViewSectionObject;
+@protocol KtTableViewDelegate<UITableViewDelegate>
+
+@optional
+
+/**
+ * 选择一个cell的回调，并返回被选择cell的数据结构和indexPath
+ */
+- (void)didSelectObject:(id)object atIndexPath:(NSIndexPath*)indexPath;
+
+- (UIView *)headerViewForSectionObject:(KtTableViewSectionObject *)sectionObject atSection:(NSInteger)section;
+
+// 将来可以有 cell 的编辑，交换，左滑等回调
+
+// 这个协议继承了UITableViewDelegate ，所以自己做一层中转，VC 依然需要实现某些代理方法。
+
+@end
+
+@interface KtBaseTableView : UITableView<UITableViewDelegate>
+
+@property (nonatomic, assign) id<KtTableViewDataSource> ktDataSource;
+
+@property (nonatomic, assign) id<KtTableViewDelegate> ktDelegate;
+
+@end
---
+++ KtTableView/KtTableView/KtBaseTableView.m
@@ -0,0 +1,80 @@
+//
+//  KtBaseTableView.m
+//  KtTableView
+//
+//  Created by baidu on 16/4/13.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+#import "KtBaseTableView.h"
+#import "KtBaseTableViewCell.h"
+#import "KtTableViewSectionObject.h"
+
+@implementation KtBaseTableView
+
+- (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style {
+    self = [super initWithFrame:frame style:style];
+    if (self) {
+        self.autoresizingMask =  UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        self.separatorColor = [UIColor clearColor];
+        self.showsVerticalScrollIndicator = YES;
+        self.showsHorizontalScrollIndicator = NO;
+        self.sectionHeaderHeight = 0;
+        self.sectionFooterHeight = 0;
+        self.delegate = self;
+    }
+    return self;
+}
+
+- (void)setKtDataSource:(id<KtTableViewDataSource>)ktDataSource {
+    if (_ktDataSource != ktDataSource) {
+        _ktDataSource = ktDataSource;
+        self.dataSource = ktDataSource;
+    }
+}
+
+#pragma mark - UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath {
+    id<KtTableViewDataSource> dataSource = (id<KtTableViewDataSource>)tableView.dataSource;
+    
+    KtTableViewBaseItem *object = [dataSource tableView:tableView objectForRowAtIndexPath:indexPath];
+    Class cls = [dataSource tableView:tableView cellClassForObject:object];
+    
+    return [cls tableView:tableView rowHeightForObject:object];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([self.ktDelegate respondsToSelector:@selector(didSelectObject:atIndexPath:)]) {
+        id<KtTableViewDataSource> dataSource = (id<KtTableViewDataSource>)tableView.dataSource;
+        id object = [dataSource tableView:tableView objectForRowAtIndexPath:indexPath];
+        [self.ktDelegate didSelectObject:object atIndexPath:indexPath];
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    }
+    else if ([self.ktDelegate respondsToSelector:@selector(tableView:didSelectRowAtIndexPath:)]) {
+        [self.ktDelegate tableView:tableView didSelectRowAtIndexPath:indexPath];
+    }
+}
+
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if ([self.ktDelegate respondsToSelector:@selector(headerViewForSectionObject:atSection:)]) {
+        id<KtTableViewDataSource> dataSource = (id<KtTableViewDataSource>)tableView.dataSource;
+        KtTableViewSectionObject *sectionObject = [((KtTableViewDataSource *)dataSource).sections objectAtIndex:section];
+        
+        return [self.ktDelegate headerViewForSectionObject:sectionObject atSection:section];
+    }
+    else if ([self.ktDelegate respondsToSelector:@selector(tableView:viewForHeaderInSection:)]) {
+        return [self.ktDelegate tableView:tableView viewForHeaderInSection:section];
+    }
+    return nil;
+}
+
+#pragma mark - 传递原生协议
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([self.ktDelegate respondsToSelector:@selector(tableView:willDisplayCell:forRowAtIndexPath:)]) {
+        [self.ktDelegate tableView:tableView willDisplayCell:cell forRowAtIndexPath:indexPath];
+    }
+}
+@end
