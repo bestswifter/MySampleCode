76487f7975b91d92ae3aaf2091530ea35c46cea0
zhangxingyu <zhangxingyu@baidu.com>
2016-05-15 18:18:51 +0800

    封装网络请求

---
+++ KtTableView/KtTableView/KtBaseModel/KtBaseServerAPI.h
@@ -0,0 +1,112 @@
+//
+//  KtBaseServerAPI.h
+//  KtTableView
+//
+//  Created by baidu on 16/5/13.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+/*!
+ @brief API访问的处理状态，IDPServerAPI.state
+ */
+typedef enum {
+    KT_PROC_STAT_CREATED = 1,
+    KT_PROC_STAT_LOADING,
+    KT_PROC_STAT_SUCCEED,
+    KT_PROC_STAT_FAILED,
+    KT_PROC_STAT_CANCELLED
+} KtProcessStateEnum;
+
+/*!
+ @brief Http请求方法
+ @discussion POST方法
+ @discussion GET方法ß
+ */
+typedef enum {
+    KT_REQUEST_POST,   // POST方法
+    KT_REQUEST_GET,    // GET方法
+} KtHttpRequestMethod;
+
+// ServerAPI 的回调
+@class KtBaseServerAPI;
+typedef void(^KtServerAPICompletionBlock)(KtBaseServerAPI *);
+
+@interface KtBaseServerAPI : NSObject
+
+@property (nonatomic, strong) AFHTTPSessionManager *manager;
+
+// request
+@property (nonatomic, readonly) NSString *server;   // 形如 www.address.of.api，可兼容前面带“http://”、以及后面带“/”的情况
+@property (nonatomic, copy) NSString *api;  // 形如 /some/api/to/access，可兼容前面不带“/”的情况
+@property (nonatomic, readonly) NSDictionary *reqParams;    // 请求参数
+@property (nonatomic, readonly) NSDictionary *files;    // 请求文件
+@property (nonatomic, copy) KtServerAPICompletionBlock userBlock;  // serverapi回调的block
+
+// response
+@property (nonatomic, retain) NSError *error; // code为IDPErrorEnum类型，服务器返回存放在userInfo的errno和errmsg里
+@property (nonatomic, retain) NSDictionary *jsonData;   // 解析出的JSON
+@property (nonatomic, retain) NSData *rawData;          // 原始data，用于二进制结果
+@property (nonatomic, readonly) NSString *rawString;    // 原始字符串，用于文本结果，lazyloading
+@property (nonatomic, retain) id protobufParserData;    // 服务器返回的protobuf解析后的数据
+
+// state
+@property (nonatomic, assign) KtProcessStateEnum state;
+
+// 性能信息
+@property (nonatomic, readonly) CFTimeInterval startTime;   // 请求发起时间
+@property (nonatomic, readonly) CFTimeInterval netCost;     // 网络耗时
+
+- (id)initWithServer:(NSString *)server;
+
+/*!
+ @brief 访问API，子类不需要重载
+ @discussion 本方法可重复调用，会自动中止当前请求（但并不调用用户回调）
+ @discussion 回调调用时机：请求失败、请求被中止、请求成功
+ @discussion 回调一般是根据state访问error或者parsedData，并关掉菊花
+ 
+ @param api    形如 /some/api/to/access，可兼容前面不带“/”的情况
+ @param params 参数
+ @param files  文件
+ @param requestMethod KtHttpRequestMethod(GET/POST)
+ @param block  KtServerAPICompletionBlock 回调
+ 
+ */
+- (void)accessAPI:(NSString *)api
+       WithParams:(NSDictionary *)params
+            files:(NSDictionary *)files
+    requestMethod:(KtHttpRequestMethod)method
+  completionBlock:(KtServerAPICompletionBlock)block;
+
+/*!
+ @brief 访问API，子类不需要重载 (DEFAULT POST METHOD)
+ 
+ @param api    形如 /some/api/to/access，可兼容前面不带“/”的情况
+ @param params 参数
+ @param files  文件
+ @param block  KtServerAPICompletionBlock 回调
+ 
+ */
+- (void)accessAPI:(NSString *)api
+       WithParams:(NSDictionary *)params
+            files:(NSDictionary *)files
+  completionBlock:(KtServerAPICompletionBlock)block;
+
+/*!
+ @brief 访问API, 省略files参数的版本
+ 
+ @param api    形如 /some/api/to/access，可兼容前面不带“/”的情况
+ @param params 参数
+ @param block  KtServerAPICompletionBlock 回调
+ 
+ */
+- (void)accessAPI:(NSString *)api
+       WithParams:(NSDictionary *)params
+  completionBlock:(KtServerAPICompletionBlock)block;
+
+- (void)cancel;
+- (void)refresh;
+
+@end
---
+++ KtTableView/KtTableView/KtBaseModel/KtBaseServerAPI.m
@@ -0,0 +1,159 @@
+//
+//  KtBaseServerAPI.m
+//  KtTableView
+//
+//  Created by baidu on 16/5/13.
+//  Copyright © 2016年 zxy. All rights reserved.
+//
+
+#import "KtBaseServerAPI.h"
+
+@interface KtBaseServerAPI ()
+
+@property (nonatomic, retain) NSString *server;
+@property (nonatomic, retain) NSDictionary *reqParams;
+@property (nonatomic, retain) NSDictionary *files;
+@property (nonatomic, assign) CFTimeInterval startTime;
+@property (nonatomic, assign) CFTimeInterval netCost;
+@property (nonatomic, strong) NSURLSessionDataTask *task;
+
+@end
+
+@implementation KtBaseServerAPI
+
+@synthesize rawString = _rawString;
+
+- (id)initWithServer:(NSString *)server {
+    self = [super init];
+    if (self) {
+        if (![server hasPrefix:@"http://"]) {
+            server = [@"http://" stringByAppendingString:server];
+        }
+        if ([server hasSuffix:@"/"]) {
+            server = [server substringToIndex:server.length - 1];
+        }
+        self.server = server;
+    }
+    return self;
+}
+
+#pragma -mark accessAPI
+
+- (void)accessAPI:(NSString *)api
+       WithParams:(NSDictionary *)params
+            files:(NSDictionary *)files
+    requestMethod:(KtHttpRequestMethod)method
+  completionBlock:(KtServerAPICompletionBlock)block {
+    if (!self.manager) {
+        // 应该获取一个全局唯一的 manager
+        self.manager = [AFHTTPSessionManager manager];
+        self.manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+        self.manager.responseSerializer.acceptableContentTypes = [self.manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
+    }
+    
+    if (self.state == KT_PROC_STAT_LOADING) {
+        [self cancel];
+    }
+    
+    self.netCost = 0; // 网络请求统计
+    // 赋值
+    if (![api hasPrefix:@"/"]) {
+        self.api = [@"/" stringByAppendingString:api];
+    } else {
+        self.api = api;
+    }
+    self.reqParams = params;
+    self.files = files;
+    self.userBlock = block;
+    
+    // 重置状态
+    self.state = KT_PROC_STAT_CREATED;
+    self.error = nil;
+    self.jsonData = nil;
+    self.rawData = nil;
+    self.rawString = nil;
+    
+    // 统计时间
+    self.startTime = CFAbsoluteTimeGetCurrent();
+    
+    // 允许子类调用自己的方法，附加请求参数
+    
+    // 发起请求
+    NSString *url = [self.server stringByAppendingString:self.api];
+    if (method == KT_REQUEST_POST) {
+        self.task = [self.manager POST:url parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            self.netCost = CFAbsoluteTimeGetCurrent() - self.startTime; // 统计网络请求时间
+            self.rawData = (NSData *)responseObject;
+            self.jsonData = [NSJSONSerialization JSONObjectWithData:self.rawData options:NSJSONReadingMutableContainers error:nil];
+            self.state = KT_PROC_STAT_SUCCEED;
+            
+            if (self.userBlock) {
+                self.userBlock(self);
+            }
+        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            self.state = KT_PROC_STAT_FAILED;
+        }];
+    }
+    else {
+        // GET
+    }
+    self.state = KT_PROC_STAT_LOADING; // 状态设置为开始请求
+}
+
+- (void)accessAPI:(NSString *)api
+       WithParams:(NSDictionary *)params
+            files:(NSDictionary *)files
+  completionBlock:(KtServerAPICompletionBlock)block {
+    [self accessAPI:api
+         WithParams:params
+              files:files
+      requestMethod:KT_REQUEST_POST
+    completionBlock:block];
+}
+
+- (void)accessAPI:(NSString *)api
+       WithParams:(NSDictionary *)params
+  completionBlock:(KtServerAPICompletionBlock)block {
+    [self accessAPI:api WithParams:params files:nil completionBlock:block];
+}
+
+#pragma -mark 刷新与取消
+// 刷新请求
+- (void)refresh {
+    [self accessAPI:self.api
+         WithParams:self.reqParams
+              files:self.files
+    completionBlock:self.userBlock];
+}
+
+// 中止请求
+- (void)cancel {
+    if (self.state == KT_PROC_STAT_LOADING) {
+        self.state = KT_PROC_STAT_CANCELLED;
+        [self.task cancel];
+    } else {
+        // 提醒
+        return;
+    }
+}
+
+// lazyloading
+- (NSString *)rawString {
+    if (_rawString) {
+        return _rawString;
+    }
+    if (self.rawData) {
+        _rawString = [[[NSString alloc] initWithData:self.rawData encoding:NSUTF8StringEncoding] copy];
+    }
+    return self->_rawString;
+}
+
+- (void)setRawString:(NSString *)rawString {
+    if (self->_rawString == rawString) {
+        return;
+    }
+    
+    self->_rawString = [rawString copy];
+}
+
+@end
