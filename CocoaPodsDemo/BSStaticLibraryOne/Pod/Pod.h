89125253d3b1c431833cb6c09521af5e53803fc3
zhangxingyu <zhangxingyu@baidu.com>
2017-01-08 21:00:21 +0800

    新增一个 target，依赖另外两个

---
+++ CocoaPodsDemo/BSStaticLibraryOne/Pod/Pod.h
@@ -0,0 +1,13 @@
+//
+//  Pod.h
+//  Pod
+//
+//  Created by 张星宇 on 2017/1/8.
+//  Copyright © 2017年 bestswifter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pod : NSObject
+
+@end
---
+++ CocoaPodsDemo/BSStaticLibraryOne/Pod/Pod.m
@@ -0,0 +1,13 @@
+//
+//  Pod.m
+//  Pod
+//
+//  Created by 张星宇 on 2017/1/8.
+//  Copyright © 2017年 bestswifter. All rights reserved.
+//
+
+#import "Pod.h"
+
+@implementation Pod
+
+@end
