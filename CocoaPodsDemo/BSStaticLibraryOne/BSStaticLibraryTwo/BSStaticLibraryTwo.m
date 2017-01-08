daff94d2b9f295cf156328c09f2cac07448884f2
zhangxingyu <zhangxingyu@baidu.com>
2017-01-08 18:31:50 +0800

    新增一个 target

---
+++ CocoaPodsDemo/BSStaticLibraryOne/BSStaticLibraryTwo/BSStaticLibraryTwo.m
@@ -0,0 +1,13 @@
+//
+//  BSStaticLibraryTwo.m
+//  BSStaticLibraryTwo
+//
+//  Created by 张星宇 on 2017/1/8.
+//  Copyright © 2017年 bestswifter. All rights reserved.
+//
+
+#import "BSStaticLibraryTwo.h"
+
+@implementation BSStaticLibraryTwo
+
+@end
